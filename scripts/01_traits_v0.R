251b00ae265c7fc8fa939ed66f3b9569fe13f80c
eliasb03 <eliasbowmanschool@gmail.com>
2024-03-18 15:00:25 -0700

    Created trait script

---
+++ scripts/01_traits_v0.R
@@ -0,0 +1,9 @@
+#############
+# BIOL 403 503 Project
+# Script: 01_traits_v0
+# By: Lydia, Elias
+# 2024-03-18
+# This script will organize functional traits based on the data from Weigel (2022) paper
+#############
+
+traits <- read.csv("data/msystems.01422-21-s0005.csv")
