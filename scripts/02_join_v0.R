704710e6a2becf794a0e4883eaaee39dc03483c5
eliasb03 <eliasbowmanschool@gmail.com>
2024-03-20 17:10:33 -0700

    Created two new datasets, which are dereived from our original merged dataset. They should contain all relevant samples for either question, the meristem vs tip and the macro vs nereo. They also have some other info I included just in case but its easy to add or substract columns by modifying the select()

--- scripts/02_join_v0.R
+++ scripts/02_join_v0.R
@@ -17,11 +17,45 @@
 # My (elias') thought: either inner_join - and work with only the ones that have both
 # or, we could try left join, and keep all the observations from the original data, and throw out non-relevant trait_data
 
-unique(merged )
-
 all_merged <- left_join(community_data, trait_data, by = "lowest_rank")
 
+# Creating two sets of data one for meristem v tip and one for host type
+# In either case filtering will need to happen
+## Filtering for relevant samples
+  # In species_data - only want macro and nereo (maybe control)
+  # In blade_data - only want nereo base and nereo tip (whats the differnec between "tip" and "tip_swab")
+## Filtering for relevant sites 
+  # Either question can only be asked using data from specific sites - need to filter for these locations
 
-# two sets of data one for meristem v and one for host type
-# lowest_rank, tip or mersitem, asv_abundance, n_cycling, 
-# same as above host type (macro vs nereo) - will have to filter for
+both_species_locations <- c("Bullman", "Cape_Johnson", "Destruction_Island", 
+                            "Koitlah", "Sekiu")
+base_and_tip <- c("Nereocystis_tip", "Nereocystis_base", "Nereocystis_tip_swab",
+                  "Nereocystis_base_swab")
+
+# The below makes species_data, with only samples from the above listed locations, where both kelp species were sampled
+species_data <- all_merged %>%
+  select(description, host_common_name, location, sample_type_user, asv_abundance, lowest_rank, nitrogen_cycling) %>%
+  filter(description == "Macrocystis" | description == "Nereocystis") %>%
+  filter(location %in% both_species_locations)
+
+# The below makes blade_data, with only samples from Tatoosh and from the blade meristem and tip, also makes a new simplified 
+blade_data <- all_merged %>%
+  select(
+    description,
+    host_common_name,
+    location,
+    sample_type_user,
+    asv_abundance,
+    lowest_rank,
+    nitrogen_cycling
+  ) %>%
+  filter(description %in% base_and_tip) %>%
+  filter(location == "Tatoosh" |
+           location == "Tatoosh_Main_Beach") %>%
+  mutate(sample_type = if_else(
+    grepl("tip", description),
+    "tip",
+    if_else(grepl("base", description), "meristem", NA_character_)
+  )) 
+
+
