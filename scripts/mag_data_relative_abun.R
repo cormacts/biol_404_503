2d07f3f776a37a0012d5334019a330a34310d4f7
cormacts <cormac@zoology.ubc.ca>
2024-03-27 11:52:07 -0700

    updates to mag code and the dataframe

--- scripts/mag_data_relative_abun.R
+++ scripts/mag_data_relative_abun.R
@@ -8,29 +8,32 @@ library(vegan)
 library(plyr)
 library(qualpalr)
 library(ggpubr)
-mag_abundance <- read.csv("data/raw/metag_abun.csv")
-# lowest taxonomic unit column
-mag_data_na <- mag_data_df %>%
-  rowwise() %>%
-  mutate(lowest_rank = if_else(!is.na(Genus), Genus,
-                               if_else(!is.na(Family), Family,
-                                       if_else(!is.na(Order), Order,
-                                               if_else(!is.na(Class), Class,
-                                                       if_else(!is.na(Phylum), Phylum, NA_character_))))))
+
+mag_abundance <- read.csv("data/raw/msystems.01422-21-s0003.csv")
+trait_data <- read.csv("trait_data_w2022.csv")
+
 #replace empty with na function
 replace_empty_with_na <- function(x) {
   x[x == ""] <- NA
   return(x)
 }
 #use function
-mag_data_df <- mag_data %>%
+mag_data_na <- mag_abundance %>%
   mutate(across(everything(), replace_empty_with_na))
+# lowest taxonomic unit column
+mag_data_df <- mag_data_na %>%
+  rowwise() %>%
+  mutate(lowest_rank = if_else(!is.na(Genus), Genus,
+                               if_else(!is.na(Family), Family,
+                                       if_else(!is.na(Order), Order,
+                                               if_else(!is.na(Class), Class,
+                                                       if_else(!is.na(Phylum), Phylum, NA_character_))))))
 
 #merge dataframes
-mag_merged <- left_join(mag_data_na, trait_data, by = "lowest_rank")
+mag_merged <- left_join(mag_data_df, trait_data, by = "lowest_rank")
 
 #select columns for condensed data frame
-mag_data_all <- mag_sum %>%
+mag_data_all <- mag_merged %>%
   select(
     MAG_name, lowest_rank,
    B1_Squaxin_2019:B7_Tatoosh_2018,
@@ -44,9 +47,11 @@ pivot_mag <- mag_data_all %>%
 
 
 # create total abundance column grouping by sample
-mag_total <- pivot_mag %>% group_by(Sample) %>% mutate(sample_total = sum(count))
+mag_total <- pivot_mag %>% group_by(Sample) %>% mutate(total = sum(count))
 # create cycling abundance column grouping by sample and cycling and calculate relative abundance
-mag_total_relabun <- mag_total %>%  group_by(Sample, nitrogen_cycling) %>% mutate(nitro_total = sum(count)) %>% mutate(relabun = nitro_total/total)
+mag_total_relabun <- mag_total %>%  
+  group_by(Sample, nitrogen_cycling) %>% 
+  mutate(nitro_total = sum(count)) %>% mutate(relabun = nitro_total/total)
 
 #select for columns, pick out unique rows 
 relabun_data <- mag_total_relabun %>%
