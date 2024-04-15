7c96e3bb538089f8c00794d97bef04fe9a629a1f
cormacts <147676430+cormacts@users.noreply.github.com>
2024-04-15 16:00:44 -0700

    Update condensed_code_for_submission.R
    updated again

--- scripts/condensed_code_for_submission.R
+++ scripts/condensed_code_for_submission.R
@@ -523,32 +523,41 @@ print(b_wctest)
 
 ## comparing weigel 2022 trait data relative proportion of nitrogen cyclers to hwat we calculated in the community composition data from Weigel et al. 2019, 2022
 # read in the data
-mag_abundance <- read.csv("data/raw/metag_abun.csv")
-# make lowest taxonomic unit column
-mag_data_na <- mag_data_df %>%
-  rowwise() %>%
-  mutate(lowest_rank = if_else(!is.na(Genus), Genus,
-                               if_else(!is.na(Family), Family,
-                                       if_else(!is.na(Order), Order,
-                                               if_else(!is.na(Class), Class,
-                                                       if_else(!is.na(Phylum), Phylum, NA_character_))))))
+mag_abundance <- read.csv("data/raw/msystems.01422-21-s0003.csv") 
+trait_data <- read.csv("trait_data_w2022.csv")
+
+
 #replace empty with na function
 replace_empty_with_na <- function(x) {
   x[x == ""] <- NA
   return(x)
 }
 #use function
-mag_data_df <- mag_data %>%
+mag_data_na <- mag_abundance %>%
   mutate(across(everything(), replace_empty_with_na))
+# lowest taxonomic unit column
+mag_data_df <- mag_data_na %>%
+  rowwise() %>%
+  mutate(lowest_rank = if_else(!is.na(Genus), Genus,
+                               if_else(!is.na(Family), Family,
+                                       if_else(!is.na(Order), Order,
+                                               if_else(!is.na(Class), Class,
+                                                       if_else(!is.na(Phylum), Phylum, NA_character_))))))
 
 #merge dataframes
-mag_merged <- left_join(mag_data_na, trait_data, by = "lowest_rank")
+mag_merged <- left_join(mag_data_df, trait_data, by = "lowest_rank")
 
+mag_merged <- mag_merged %>% 
+  mutate(
+    nitrogen_cycling = recode_factor(nitrogen_cycling,
+                                     "N" = "No",
+                                     "Y" = "Yes"),
+    nitrogen_cycling = fct_explicit_na(nitrogen_cycling, "Unknown"))
 #select columns for condensed data frame
-mag_data_all <- mag_sum %>%
+mag_data_all <- mag_merged %>%
   select(
     MAG_name, lowest_rank,
-   B1_Squaxin_2019:B7_Tatoosh_2018,
+    B1_Squaxin_2019:B7_Tatoosh_2018,
     nitrogen_cycling)
 #make dataframe longer and put sites/samples in a column, create a count column
 pivot_mag <- mag_data_all %>% 
@@ -557,10 +566,13 @@ pivot_mag <- mag_data_all %>%
     names_to = "Sample", 
     values_to = "count")
 
+
 # create total abundance column grouping by sample
-mag_total <- pivot_mag %>% group_by(Sample) %>% mutate(sample_total = sum(count))
+mag_total <- pivot_mag %>% group_by(Sample) %>% mutate(total = sum(count))
 # create cycling abundance column grouping by sample and cycling and calculate relative abundance
-mag_total_relabun <- mag_total %>%  group_by(Sample, nitrogen_cycling) %>% mutate(nitro_total = sum(count)) %>% mutate(relabun = nitro_total/total)
+mag_total_relabun <- mag_total %>%  
+  group_by(Sample, nitrogen_cycling) %>% 
+  mutate(nitro_total = sum(count)) %>% mutate(relabun = nitro_total/total)
 
 #select for columns, pick out unique rows 
 relabun_data <- mag_total_relabun %>%
@@ -568,16 +580,45 @@ relabun_data <- mag_total_relabun %>%
   unique()
 
 #plot
-sp_stackplot <- ggplot(relabun_data, aes(fill=nitrogen_cycling, y=relabun, x=Sample)) + 
+mag_stackplot <- ggplot(relabun_data, aes(fill=nitrogen_cycling, y=relabun, x=Sample)) + 
   geom_bar(position="stack", stat="identity")+
-  labs(x = "Samples", y = "Relative Abundance of Taxa", color = "Nitrogen Cycling") +
+  labs(x = "Samples", y = "Relative Abundance", color = "Nitrogen Cycling") +
   theme(strip.text = element_text(face = "italic"),
         axis.text.x = element_text(colour = "grey20", size = 12)) +
   theme_bw() +
   scale_fill_manual(values = c("lightblue", "yellow1", "violet"))
-# save plot as a png
-png(file = "figures/mag_proportion_taxaplot.png")
-sp_stackplot
+
+png(file = "figures/mag_taxaplot.png")
+mag_stackplot
 dev.off()
+
+##### stats for comparing mag relative abundance data to 16s data ####
+
+#creat dataframes from each dataset (mag data and 16s data) with the proportions of nitrogen cyclers
+
+s_proportions <- filter(plot_sp_proportions,species == "Nereocystis", nitrogen_cycling == "proportion_Y_22") 
+
+relabun_Y <- filter(relabun_data, nitrogen_cycling == "YES")
+
+proportion_16s <- s_proportions$proportions_22
+proportion_mag <- relabun_data$relabun
+# Perform Welch's t-test
+ttest_mag_16s <- t.test(proportion_16s, proportion_mag, var.equal = FALSE)
+
+# Print the result
+print(ttest_mag_16s)
+
+
+# Didn't check our assumptions but added this in case, also p<0.5
+utest_mag_16s <- wilcox.test(proportion_16s, proportion_mag)
+print(utest_mag_16s)
+
+#t = 2.765, df = 40.312, p-value = 0.008546
+# there is a statistically significant difference between the relative abundances of nitrogen cyclers in
+# the metagenomic data vs the 16S data for community composition of functional traits. Therefore we can
+# reject the null hypothesis that these two groups are the same. 
+
+#This means that the functional trait composition
+# data between these two groups cannot be compared because there are two many unknowns in the 16s data
 
 
